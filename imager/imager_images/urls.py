08ae5917319eda1dbc95a0a45a9b469e3469bcb8
Robert Haskell <robertwhaskell@gmail.com>
2015-03-12 14:34:41 -0700

    set up skeleton for stream page

--- imager/imager_images/urls.py
+++ imager/imager_images/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from imager_images import views
 
 urlpatterns = patterns('',
-    url(r'^library$', views.library, name='profile-library')
+    url(r'^library/(?P<pk>\d+)$', views.library, name='profile-library'),
+    url(r'^stream/(?P<pk>\d+)$', views.stream, name='profile-stream')
     )
--- imager/imager_images/views.py
+++ imager/imager_images/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from profiles.models import ImagerProfile
 
 # Create your views here.
-def library(request):
+def library(request, pk):
+    pk = int(pk)
     return render(request,
                   'imager_images/library.html',
                   {
-                    'Photos': ImagerProfile.objects.all()[2].show_photos(),
-                    'Albums': ImagerProfile.objects.all()[2].show_albums(),
-                    'Profile': ImagerProfile.objects.all()[2]
+                    'Photos': ImagerProfile.objects.all()[pk].show_photos(),
+                    'Albums': ImagerProfile.objects.all()[pk].show_albums(),
+                    'Profile': ImagerProfile.objects.all()[pk]
+                  })
+
+def stream(request, pk):
+    pk = int(pk)
+    return render(request,
+                  'imager_images/profilestream.html',
+                  {
+                    'Photos': ImagerProfile.objects.all()[pk].show_photos(),
+                    'Albums': ImagerProfile.objects.all()[pk].show_albums(),
+                    'Profile': ImagerProfile.objects.all()[pk]
                   })
