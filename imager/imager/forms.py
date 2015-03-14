2e497e2b490e505108738a60f2dc6c4289652548
Robert Haskell <roberthaskell1@Roberts-iMac.local>
2015-03-14 02:29:29 -0700

    And setting the groundwork for doing the profile page

--- imager/imager/forms.py
+++ imager/imager/forms.py
@@ -14,9 +14,14 @@ class AlbumForm(ModelForm):
     class Meta:
         model = Album
         fields = ['title', 'description', 'photos', 'cover_photo']
-        
+
     def __init__(self, *args, **kwargs):
         super(AlbumForm, self).__init__(*args, **kwargs)
         profile = ImagerProfile.objects.get(user=self.initial['user'])
         self.fields['photos'].queryset = Photo.objects.filter(profile=profile)
         self.fields['cover_photo'].queryset = Photo.objects.filter(profile=profile)
+
+class ProfileForm(ModelForm):
+
+	class Meta:
+		model = ImagerProfile
