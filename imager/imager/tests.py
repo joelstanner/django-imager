90b1c8306211546607cb56a79731d6686168ec23
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-14 18:35:56 -0700

    form template tests

--- imager/imager/tests.py
+++ imager/imager/tests.py
@@ -1,10 +1,8 @@
 from __future__ import print_function
 from django.test import TestCase
 from django.contrib.auth.models import User
-from imager_images.models import Photo
+from imager_images.models import Photo, Album
 from django.test import Client
-
-
 import factory
 
 
@@ -15,6 +13,7 @@ class UserFactory(factory.django.DjangoModelFactory):
         django_get_or_create = ('username',)
 
     username = 'Bob'
+    password = factory.PostGenerationMethodCall('set_password', 'password')
 
 
 class PhotoFactory(factory.django.DjangoModelFactory):
@@ -24,6 +23,14 @@ class PhotoFactory(factory.django.DjangoModelFactory):
 
     profile = UserFactory.create(username='Bobby').ImagerProfile
     photo = 'picture.jpeg'
+
+
+class AlbumFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Album
+
+    profile = UserFactory.create(username='Freddy').ImagerProfile
 
 
 class TestHomepageViews(TestCase):
@@ -72,3 +79,108 @@ class TestRegistrationViews(TestCase):
                           'password2': 'test'}
                          )
         self.assertEqual(len(User.objects.all()), 1)
+
+
+class TestImagerViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.bob = UserFactory.create()
+        self.alice = UserFactory.create(username='Alice')
+        self.IP_bob = self.bob.ImagerProfile
+        self.bobphoto = PhotoFactory.create(profile=self.bob.ImagerProfile,
+                                            title="bob photo",
+                                            published='pb')
+        self.bobalbum = AlbumFactory.create(profile=self.bob.ImagerProfile,
+                                            title="bob awesome album",
+                                            published='pb')
+
+    def test_profile_update_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        response = self.client.get('/update_profile/' + str(self.bob.pk) + '/')
+        self.assertTemplateUsed(response, 'update_profile.html')
+
+    def test_profile_update_unreachable_if_loggedout(self):
+        response = self.client.get('/update_profile/' + str(self.bob.pk) + '/')
+        self.assertTemplateNotUsed(response, 'update_profile.html')
+
+    def test_PhotoUpdate_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        response = self.client.get('/update_photo/' + str(self.bobphoto.id) + '/')
+        self.assertTemplateUsed(response, 'update_photo.html')
+
+    def test_PhotoUpdate_unreachable_if_loggedout(self):
+        response = self.client.get('/update_photo/' + str(self.bobphoto.id) + '/')
+        self.assertTemplateNotUsed(response, 'update_photo.html')
+
+    def test_PhotoDelete_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        response = self.client.get('/delete_photo/' + str(self.bobphoto.id) + '/')
+        self.assertTemplateUsed(response, 'delete_photo.html')
+
+    def test_PhotoDelete_unreachable_if_loggedout(self):
+        response = self.client.get('/delete_photo/' + str(self.bobphoto.id) + '/')
+        self.assertTemplateNotUsed(response, 'delete_photo.html')
+
+    def test_PhotoDelete_actually_deletes_photo(self):
+        self.client.login(username='Bob', password='password')
+        bobphoto_id = self.bobphoto.id
+        self.client.post('/delete_photo/' + str(self.bobphoto.id) + '/')
+        response = self.client.get('/delete_photo/' + str(bobphoto_id) + '/')
+        self.assertTemplateNotUsed(response, 'delete_photo.html')
+
+    def test_PhotoDelete_not_usable_on_other_users_photo(self):
+        self.client.login(username='Bob', password='password')
+        self.alicephoto = PhotoFactory.create(profile=self.alice.ImagerProfile,
+                                              title="alice cool shot",
+                                              published='pb')
+        response = self.client.post('/delete_photo/' + str(self.alicephoto.id) + '/')
+        self.assertTrue(self.alicephoto.id)
+        self.assertTemplateNotUsed(response, 'delete_photo.html')
+
+    def test_AlbumCreate_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        response = self.client.get('/add_album/')
+        self.assertTemplateUsed(response, 'albums_form.html')
+
+    def test_AlbumCreate_unreachable_if_loggedout(self):
+        response = self.client.get('/add_album/')
+        self.assertTemplateNotUsed(response, 'albums_form.html')
+
+    def test_AlbumUpdate_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        self.bobalbum = AlbumFactory.create(profile=self.bob.ImagerProfile)
+        response = self.client.get('/update_album/' + str(self.bobalbum.pk) + '/')
+        self.assertTemplateUsed(response, 'update_album.html')
+
+    def test_AlbumUpdate_unreachable_if_loggedout(self):
+        self.bobalbum = AlbumFactory.create(profile=self.bob.ImagerProfile)
+        response = self.client.get('/update_album/' + str(self.bobalbum.pk) + '/')
+        self.assertTemplateNotUsed(response, 'update_album.html')
+
+    def test_AlbumDelete_displays_correct_template(self):
+        self.client.login(username='Bob', password='password')
+        self.bobalbum = AlbumFactory.create(profile=self.bob.ImagerProfile)
+        response = self.client.get('/delete_album/' + str(self.bobalbum.id) + '/')
+        self.assertTemplateUsed(response, 'delete_album.html')
+
+    def test_AlbumDelete_unreachable_if_loggedout(self):
+        self.bobalbum = AlbumFactory.create(profile=self.bob.ImagerProfile)
+        response = self.client.get('/delete_album/' + str(self.bobalbum.id) + '/')
+        self.assertTemplateNotUsed(response, 'delete_album.html')
+
+    def test_AlbumDelete_actually_deletes_album(self):
+        self.client.login(username='Bob', password='password')
+        bobalbum_id = self.bobalbum.id
+        self.client.post('/delete_album/' + str(self.bobalbum.id) + '/')
+        response = self.client.get('/album_photo/' + str(bobalbum_id) + '/')
+        self.assertTemplateNotUsed(response, 'delete_album.html')
+
+    def test_AlbumDelete_not_usable_on_other_users_album(self):
+        self.client.login(username='Bob', password='password')
+        self.alicealbum = AlbumFactory.create(profile=self.alice.ImagerProfile,
+                                              title="alice cool album",
+                                              published='pb')
+        response = self.client.post('/delete_album/' + str(self.alicealbum.id) + '/')
+        self.assertTrue(self.alicealbum.id)
+        self.assertTemplateNotUsed(response, 'delete_album.html')
