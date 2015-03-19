878576ca4d94f31ffc2e405e5fc37f267e20d620
Robert Haskell <robertwhaskell@gmail.com>
2015-03-18 18:43:33 -0700

    changed production for server

--- imager/imager/production.py
+++ imager/imager/production.py
@@ -1,7 +1,7 @@
 from settings import *
 import os
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['ec2-54-148-71-55.us-west-2.compute.amazonaws.com', 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
