b89fd250d7e2a9f05d67f77344e319640d3c3a12
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-19 19:40:18 -0700

    getting selenium tests going

--- imager/imager/tests_selenium.py
+++ imager/imager/tests_selenium.py
@@ -1,14 +1,30 @@
 from __future__ import print_function
 
-from django.test import TestCase, LiveServerTestCase
+from django.test import LiveServerTestCase
 from selenium import webdriver
 
-
+import os
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
 from django.contrib.auth.models import User
 from imager_images.models import Photo, Album
-from django.test import Client
 import factory
+
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+        django_get_or_create = ('username',)
+
+    username = 'Bob'
+    password = factory.PostGenerationMethodCall('set_password', 'password')
 
 
 class TestLogin(LiveServerTestCase):
@@ -25,3 +41,14 @@ class TestLogin(LiveServerTestCase):
         self.driver.get(self.live_server_url)
         self.assertIn('Login', self.driver.page_source)
         self.assertIn('Register', self.driver.page_source)
+
+    def test_authorized_index(self):
+        self.driver.find_element_by_link_text("Login").click()
+        username_field = self.driver.find_element_by_id("id_username")
+        username_field.send_keys("Bob")
+        password_field = self.driver.find_element_by_id("id_password")
+        password_field.send_keys("password")
+        self.assertIn('Bob', self.driver.page_source)
+        
+        
+        
