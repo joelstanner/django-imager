fde0efe386b923e6a92cb53de047c1c77ed089d8
Joel Stanner <joel@joelstanner.com>
2015-09-21 19:33:31 -0700

    linting

--- imager/imager/views.py
+++ imager/imager/views.py
@@ -8,10 +8,10 @@ def home(request):
             request,
             'index.html',
             {'random_photo': Photo.random_image.all()[0].photo.url}
-            )
+        )
     except IndexError:
         return render(
             request,
             'index.html',
             {'random_photo': '/media/default_stock_photo_640_360.jpg'}
-            )
+        )
